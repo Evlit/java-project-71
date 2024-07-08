da5f713e42f828a9537a606fc2e913bc19ace0c5
Eugeniy Litvinenko <evlit23@gmail.com>
2024-07-08 20:54:21 +0300

    Step 5

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Evlit/java-project-71/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Evlit/java-project-71/actions)
+
+### Project asciinemas:
+[![asciicast](https://asciinema.org/a/jjgBp4BTh6N50bbcVRguuuYTY.svg)](https://asciinema.org/a/jjgBp4BTh6N50bbcVRguuuYTY)
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,29 +4,28 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-import java.io.File;
+import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
-public class App implements Runnable {
+public class App implements Callable<String> {
     @Option(names = { "-f", "--format" }, paramLabel = "format", description = "output format [default: stylish]")
     String format;
 
-    @Parameters(paramLabel = "filepath1", description = "path to first file")
-    File filepath1;
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    String filepath1;
 
-    @Parameters(paramLabel = "filepath2", description = "path to second file")
-    File filepath2;
-
-//    @Option(names = { "-h", "--help" }, usageHelp = true, description = "display a help message")
-//    private boolean helpRequested = false;
-
-    public static void main(String... args) {
-        CommandLine.run(new App(), System.err, args);
-    }
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    String filepath2;
 
     @Override
-    public void run() {
-        System.out.println("Hello World!");
+    public String call() throws Exception { // your business logic goes here...
+        System.out.println(Differ.generate(filepath1, filepath2));
+        return null;
+    }
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
